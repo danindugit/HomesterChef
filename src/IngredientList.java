4585304f6dfc14b5e794e2215a7266a8f953a240
danindugit <danindum@outlook.com>
2020-06-12 11:39:27 -0400

    Added all classes.

---
+++ src/IngredientList.java
@@ -0,0 +1,13 @@
+
+public class IngredientList {
+
+	public IngredientList() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/User.java
@@ -0,0 +1,13 @@
+
+public class User {
+
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
