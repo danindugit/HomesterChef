1c554bedf90155c1a3a588cd6b0b47f662ca5b58
danindugit <danindum@outlook.com>
2020-06-13 19:19:54 -0400

    Created the self-testing main.

--- src/User.java
+++ src/User.java
@@ -5,15 +5,15 @@ import java.util.Arrays;
  *Date: 2020 06 12
  *Desc.: A class for a user object, which includes private data for a user's name, list of ingredients, and list of recipes.
  *Method List: 
- *		public User()
- *		public String getName()
- *		public void setName(String name)
- *		public IngredientList getInventory()
- *		public void setInventory(IngredientList inventory)
- *		public RecipeList getRecipes()
- *		public void setRecipes(RecipeList recipes)
- *		public void makeRecipe(Recipe r) 
- *		public static void main(String[] args)
+ *		public User() *
+ *		public String getName() *
+ *		public void setName(String name) *
+ *		public IngredientList getInventory() *
+ *		public void setInventory(IngredientList inventory) *
+ *		public RecipeList getRecipes() *
+ *		public void setRecipes(RecipeList recipes) *
+ *		public void makeRecipe(Recipe r) *
+ *		public static void main(String[] args) *
  */
 
 public class User {
@@ -67,7 +67,7 @@ public class User {
 	public void setRecipes(RecipeList recipes) {
 		this.recipes = recipes;
 	}
-	
+
 	//method that adjusts quantity of user inventory when a recipe is made
 	public void makeRecipe(Recipe r) {
 		for (int i = 0; i < r.getIngNeeded().getSize(); i++) {  //loop through all of the required ingredients of the recipe
@@ -78,12 +78,82 @@ public class User {
 
 	@Override
 	public String toString() {
-		return "User [name=" + this.getName() + ", inventory=" + Arrays.toString(this.getInventory().getList()) + ", recipes=" + Arrays.toString(this.getRecipes().getList()) + "]";
+		return "User [name=" + this.getName() + ", inventory= \n" + this.getInventory().toString() + ", recipes= \n" + this.getRecipes().toString() + "]";
 	}
-
+	//self-testing main
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		User u = new User ();//creating a new user object
+		u.setName("Dan");
 
+		IngredientList iList = new IngredientList ();//creating a new ingredientList
+		//creating salt and adding to the list
+		Ingredient salt = new Ingredient();
+		salt.setName("Salt");
+		salt.setQuantity(10);
+		iList.insert(salt);
+		//creating pepper and sugar and adding it to the list
+		Ingredient pepper = new Ingredient ("Pepper", 15);
+		Ingredient sugar = new Ingredient ("Sugar", 20);
+		iList.insert(pepper);
+		iList.insert(sugar);
+		u.setInventory(iList);  //setting iList as the user's Inventory
+		//creating a recipe list
+		RecipeList rList = new RecipeList();
+		//a recipe that the user can't make
+		Recipe r1 = new Recipe();
+		r1.setName("Cannot make");
+		//making ingredients and adding it to the recipe's required ingredients
+		IngredientList recIng1 = new IngredientList();
+		Ingredient recSalt1 = new Ingredient ("Salt", 10);
+		Ingredient recPepper1 = new Ingredient ("Pepper", 15);
+		Ingredient recSugar1 = new Ingredient ("Sugar", 21);  //user only has 20 sugar, this recipe requires 21
+		recIng1.insert(recSalt1);
+		recIng1.insert(recPepper1);
+		recIng1.insert(recSugar1);
+		r1.setIngNeeded(recIng1);//setting the required ingredients list for r1 as recIng1
+		r1.setInstructions("Just cook and eat.");  //setting r1's recipe instructions
+
+		//a recipe that the user can make
+		Recipe r2 = new Recipe();
+		r2.setName("Can make");
+		//making ingredients and adding it to the recipe's required ingredients
+		IngredientList recIng2 = new IngredientList();
+		Ingredient recSalt2 = new Ingredient ("Salt", 10);
+		Ingredient recPepper2 = new Ingredient ("Pepper", 15);
+		Ingredient recSugar2 = new Ingredient ("Sugar", 20);
+		recIng2.insert(recSalt2);
+		recIng2.insert(recPepper2);
+		recIng2.insert(recSugar2);
+		r2.setIngNeeded(recIng2);//setting the required ingredients list for r2 as recIng2
+		r2.setInstructions("Just cook and eat2.");  //setting r2's recipe instructions
+		//adding the 2 recipes to the user's recipe list and setting rList as the user's recipe list
+		rList.insert(r1);
+		rList.insert(r2);
+		u.setRecipes(rList);
+		//displaying the user's initial info
+		System.out.println("User's Initial Info:");
+		System.out.println(u.toString());
+		//testing the compareToInv method
+		System.out.println("Trying to make recipe 1:");
+		if (r1.compareToInv(u)==false) {
+			System.out.println("You don't have sufficient ingredients.");
+		}
+		else {
+			System.out.println("You have sufficient ingredients.");
+		}
+		
+		System.out.println("Trying to make recipe 2:");
+		if (r2.compareToInv(u)==false) {
+			System.out.println("You don't have sufficient ingredients.");
+		}
+		else {
+			System.out.println("You have sufficient ingredients.");
+			u.makeRecipe(r2);  //make the recipe, which should set the user's ingredients all to 0
+		}
+		//displaying the user's final info
+		System.out.println();
+		System.out.println("User's final info:");
+		System.out.println(u.toString());
 	}
 
 }
