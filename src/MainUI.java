78039c52b30812e4a8c9e0acd47749115ba05da0
danindugit <danindum@outlook.com>
2020-06-14 12:16:01 -0400

    Declared private data and created bg image.

--- src/MainUI.java
+++ src/MainUI.java
@@ -1,3 +1,7 @@
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 /**
  * @author Danindu
  *Date: 2020 06 13
@@ -5,10 +9,21 @@
  *Method List: 
  *		
  */
-public class MainUI {
+public class MainUI extends JFrame{
+	//private data
+	private User u;
+	private ImagePicture bg;
+	private TextPicture title;
+	private JButton btnStart;
+	private int width, height;
 
 	public MainUI() {
-		// TODO Auto-generated constructor stub
+		super("Homester Chef");  //frame title
+		//initializing frame width and height
+		width =400;
+		height = 400;
+		//creating ImagePicture and TextPicture
+		bg = new ImagePicture(new ImageIcon("bg.png"));
 	}
 
 	public static void main(String[] args) {
