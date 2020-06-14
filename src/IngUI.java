73d7dd4e290eea8d47405bdb2f88d0b0631df56b
danindugit <danindum@outlook.com>
2020-06-14 15:10:32 -0400

    Made Intro comments

---
+++ src/IngUI.java
@@ -0,0 +1,27 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+/**
+ * @author Danindu
+ *Date: 2020 06 14
+ *Desc.: A class for the Ingredients GUI
+ *Method List: 
+ *		public MenuUI()
+ *		public void actionPerformed(ActionEvent e)
+ */
+
+public class IngUI extends JFrame implements ActionListener{
+
+	public IngUI() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/RecipesUI.java
@@ -0,0 +1,27 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+/**
+ * @author Danindu
+ *Date: 2020 06 14
+ *Desc.: A class for the Recipes GUI
+ *Method List: 
+ *		public MenuUI()
+ *		public void actionPerformed(ActionEvent e)
+ */
+
+public class RecipesUI extends JFrame implements ActionListener{
+
+	public RecipesUI() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
