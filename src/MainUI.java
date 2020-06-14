ae8b071118427f0805633e6093e29746e7bc5ea7
danindugit <danindum@outlook.com>
2020-06-14 12:48:30 -0400

    Completed MainUI

--- src/MainUI.java
+++ src/MainUI.java
@@ -1,3 +1,8 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -9,8 +14,9 @@ import javax.swing.JFrame;
  *Method List: 
  *		
  */
-public class MainUI extends JFrame{
+public class MainUI extends JFrame implements ActionListener{
 	//private data
+	private Color beta;
 	private ImagePicture bg;
 	private TextPicture title;
 	private JButton btnStart;
@@ -22,18 +28,47 @@ public class MainUI extends JFrame{
 		width =400;
 		height = 400;
 		
+		//creating color
+		beta = new Color(235, 64, 52);
+		
 		//creating ImagePicture and TextPicture
-		bg = new ImagePicture(new ImageIcon("bg.png"));
-		title = new TextPicture ("Welcome to Homester Chef", 180, 50);
+		bg = new ImagePicture(new ImageIcon("bg.png"),0,0);
+		title = new TextPicture ("Welcome to Homester Chef", 105, 50);
+		title.setFont(new Font("Arial", Font.BOLD, 16));
+		title.setColor(beta);
 		
 		//creating button
 		btnStart = new JButton ("Start");
+		btnStart.setBackground(beta);
+		btnStart.setForeground(Color.WHITE);
+		btnStart.addActionListener(this);
+		
+		//setting bounds
+		bg.setBounds(0, 0, width, height);
+		title.setBounds(0, 0, width, height);
+		btnStart.setBounds(125, 315, 150, 35);
+		
+		//adding to the frame
+		add(btnStart);
+		add(title);
+		add(bg);
+		
+		// set size and location of frame, making it visible
+		setSize(width,height); 
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	//method that listens to clicks
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==btnStart) {
+			//MenuUI ui = new MenuUI();
+			System.exit(0);
+		}
 		
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		MainUI ui = new MainUI();
 	}
 
 }
