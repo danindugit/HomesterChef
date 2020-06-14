5821c31e9fbe3f0587d41e330cfaac351694459a
danindugit <danindum@outlook.com>
2020-06-13 23:51:59 -0400

    Created the Main UI class and made its intro comments.

---
+++ src/MainUI.java
@@ -0,0 +1,19 @@
+/**
+ * @author Danindu
+ *Date: 2020 06 13
+ *Desc.: A class for the Main GUI
+ *Method List: 
+ *		
+ */
+public class MainUI {
+
+	public MainUI() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
